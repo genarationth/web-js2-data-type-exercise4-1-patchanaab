cbe295773db0895f1ab0e6a505e83b401ea11030
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-13 14:31:47 +0700

    exercise4

---
+++ ex4-1.js
@@ -0,0 +1,14 @@
+const products={
+    product_id: 1 ,
+    product_name:"WaterBottle",
+    quantity_in_stock:"1",
+    unit_price:1000,
+    product_description : {
+        product_brand:"Hydro Flask",
+        size_or_quantity:"900 ML",
+        taste_or_scent:"-",
+        colour:"Dark",
+        additional_description:"Water bottle by HydroFlask",
+    }
+};
+console.log(products);
